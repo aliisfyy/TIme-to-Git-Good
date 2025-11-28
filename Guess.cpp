19f8a4a13d30c7663411184dd6c7f66a681de462
Ong <ongtx0904@gmail.com>
2025-11-28 17:45:22 +0800

    first commit

---
+++ Guess.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    const int MIN = 0, MAX = 100;
+    cout << "=== Welcome to the Number Guessing Game! ===\n";
+
+    srand(time(0));
+    int number = rand() % (MAX - MIN + MIN);
+
+    int guess, attempts = 0;
+
+    while (guess != number) 
+    {
+        cout << "Guess a number between " << MIN << " and " << MAX << ": ";
+        cin >> guess;
+
+        if (guess < number)
+            cout << "Too low! Try again.\n";
+        else if (guess > number)
+            cout << "Too high! Try again.\n";
+        else {
+            cout << "Congratulations! You guessed the number " << number << " in " << attempts << " attempts.\n";
+        }
+        attempts =+ 1;
+    }
+
+    cout << "---------------------------------\n";
+
+    system("pause");
+    return 0;
+}
